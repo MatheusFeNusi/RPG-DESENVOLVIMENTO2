6e2d96d352645a72e3c2d529eca2716f6e20105c
Hiago-Maitan <hiagomaitan@gmail.com>
2019-10-31 23:01:01 -0300

    inicia api

---
+++ Login/src/services/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { constants } from "crypto";
+
+function requisicao() {
+    const urlPadrao = axios.create()
+}
+
+//http://localhost:8080/register POST
+http://localhost:8080/register/ GET
+
+
