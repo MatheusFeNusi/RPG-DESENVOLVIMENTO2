9017e819e110ef28e33dfb4abd8b0aa0b233776a
Luis Augusto Ferreira Delmondes <ladelmondes@stefanini.com>
2020-04-16 23:32:56 -0300

    feito tela de cadastro de personagem

--- Mobile/3DTermination2/app/src/main/java/com/example/a3dtermination/CadastroUser.kt
+++ Mobile/3DTermination2/app/src/main/java/com/example/a3dtermination/CadastroUser.kt
@@ -1,12 +1,18 @@
 package com.example.a3dtermination
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CadastroUser : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_user)
+    }
+    fun trocarTelaLogin(v: View){
+        val telaLogin = Intent(this,MainActivity::class.java)
+        startActivity(telaLogin)
     }
 }
