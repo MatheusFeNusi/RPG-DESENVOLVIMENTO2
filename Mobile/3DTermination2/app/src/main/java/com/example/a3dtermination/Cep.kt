9a1a93806acf399ba918216be019545012a46d26
Luis Augusto Ferreira Delmondes <ladelmondes@stefanini.com>
2020-06-22 14:06:10 -0300

    Internaciolização e dados offline

--- Mobile/3DTermination2/app/src/main/java/com/example/a3dtermination/Cep.kt
+++
@@ -1,4 +0,0 @@
-package com.example.a3dtermination
-
-// aqui mapeamos um JSON
-data class Cep(val logradouro:String, val bairro:String, val localidade:String)
--- Mobile/3DTermination2/app/src/main/java/com/example/a3dtermination/MainActivity.kt
+++ Mobile/3DTermination2/app/src/main/java/com/example/a3dtermination/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.a3dtermination
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,21 +12,33 @@ import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
+    var preferencias: SharedPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mostrarFragment()
+        val preferencias = getPreferences(Context.MODE_PRIVATE)
+        email.setText(preferencias.getString("emailOffline",""))
+        senha.setText(preferencias.getString("senhaOffline",""))
     }
     fun validacaoLongin(V:View) {
         val loginMocado = "admin"
         val senhaMocado = "admin"
-        val email = email.text.toString()
-        val senha = senha.text.toString()
-        if(email.isEmpty() && senha.isEmpty()){
+        val emailLogin = email.text.toString()
+        val senhaLogin = senha.text.toString()
+        val editor = preferencias?.edit()
+        val editor2 = preferencias?.edit()
+        val telaCadastroPerson = Intent(this, CadastroPerson::class.java)
+        if(emailLogin.isEmpty() && senhaLogin.isEmpty()){
             Toast.makeText(this, "Todos campos estão vazios", Toast.LENGTH_SHORT).show()
         } else {
-            if (loginMocado == email && senhaMocado == senha) {
-                val telaCadastroPerson = Intent(this, CadastroPerson::class.java)
+            Toast.makeText(this, "Sucesso", Toast.LENGTH_SHORT).show()
+            if (loginMocado == emailLogin && senhaMocado == senhaLogin) {
+                editor?.putString("emailOffline", emailLogin)
+                editor2?.putString("senha", senhaLogin)
+                editor?.apply()
+                editor2?.apply()
                 startActivity(telaCadastroPerson)
             } else {
                 Toast.makeText(this, "Email ou senha incorretos", Toast.LENGTH_SHORT).show()
